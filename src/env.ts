142d02a7f89bf767a99fb6ce1985921650e6121e
Sanooj Es <mail+github@sanooj.uk>
2026-02-06 02:07:03 +0530

    chore: fmt

--- src/env.ts
+++ src/env.ts
@@ -2,5 +2,5 @@ import "dotenv/config";
 
 const isInternal = process.env.SPICE_INTERNAL === "true";
 const isDev = process.env.IS_DEV === "true";
-const spicetifyBin = process.env.SPICETIFY_BIN || process.env.SPICE_BIN  || "spicetify";
+const spicetifyBin = process.env.SPICETIFY_BIN || process.env.SPICE_BIN || "spicetify";
 export const env = { isInternal, isDev, spicetifyBin };

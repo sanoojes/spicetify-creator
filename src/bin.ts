9ce29568b2b2aa035b48a18122eabba8245c28d6
Sanooj Es <mail+github@sanooj.uk>
2026-03-08 01:52:01 +0530

    feat: add version flag

--- src/bin.ts
+++ src/bin.ts
@@ -6,9 +6,15 @@ import { dev } from "@/commands/dev";
 import { update_types } from "@/commands/update-types";
 import { env } from "@/env";
 import { logger } from "@/utils/logger";
+import { version } from "@package.json" with { type: "json" };
 
 logger.debug(`Env: ${JSON.stringify(env, null, 2)}\n`);
 
 const command = new Command();
-command.addCommand(create.alias("init")).addCommand(build).addCommand(dev).addCommand(update_types);
+command
+  .version(version)
+  .addCommand(create.alias("init"))
+  .addCommand(build)
+  .addCommand(dev)
+  .addCommand(update_types);
 command.parse();
